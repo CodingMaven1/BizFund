7fcace48f5c258b54616ef7e2978ca47d41e0913
Naman Agarwal <namana019@outlook.com>
2020-06-16 23:42:18 +0530

    Only responsiveness left

--- src/ethereum/contracts/Campaign.sol
+++ src/ethereum/contracts/Campaign.sol
@@ -69,7 +69,7 @@ contract Campaign{
     }
     function finalizeRequest(uint index) public restricted{
         Request storage request = requests[index];
-        require(request.approvalCount > (approversCount/2));
+        require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
         request.recipient.transfer(request.value);
         request.complete = true;
